3050f4b17cfd0f54fa64f984bd93071247315faa
RicardoBernardo870 <77888335+RicardoBernardo870@users.noreply.github.com>
2021-03-17 18:30:21 +0000

    Update forecast.js

--- Scripts/forecast.js
+++ Scripts/forecast.js
@@ -1,8 +1,8 @@
 class Forecast {
     constructor(){
         this.key = 'WszvlhNLIwxy2K8tebIHoUAAHqeNODWi'
-        this.weatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/';
-        this.cityURI = 'https://dataservice.accuweather.com/locations/v1/cities/search';
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     }
 
     async updateCity(city) {
